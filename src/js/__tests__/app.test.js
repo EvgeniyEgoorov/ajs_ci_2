c9a24f821e34391b5d3fab302ad15d3ccb60ef42
Egorov Evgeniy <egorovzhenya@gmail.com>
2022-12-23 18:13:01 +0800

    feat: added sorting function and test for it

--- src/js/__tests__/app.test.js
+++ src/js/__tests__/app.test.js
@@ -1,10 +1,25 @@
-import healthIndicator from '../app';
+import { healthIndicator, sortCharacters } from '../app';
 
 test.each([
     [{ name: 'char1', health: 90 }, 'healthy'],
     [{ name: 'char2', health: 45 }, 'wounded'],
     [{ name: 'char3', health: 5 }, 'critical'],
-])('testing health status for %p', (object, expected) => {
+])('test health status for %p', (object, expectedResult) => {
     const result = healthIndicator(object);
-    expect(result).toBe(expected);
+    expect(result).toBe(expectedResult);
+});
+
+test('test sorting feature', () => {
+    const objectList = [
+        { name: 'мечник', health: 10 },
+        { name: 'маг', health: 100 },
+        { name: 'лучник', health: 80 },
+      ];
+    const expectedResult = [
+        { name: 'маг', health: 100 },
+        { name: 'лучник', health: 80 },
+        { name: 'мечник', health: 10 },
+      ];
+    const result = sortCharacters(objectList);
+    expect(result).toEqual(expectedResult);
 });
